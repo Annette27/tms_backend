7e5dec7dac0f89bcef8dfefbb86f92d9193eb97c
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-26 11:34:51 +0530

    Update batches.js

--- controllers/batches.js
+++ controllers/batches.js
@@ -2,7 +2,8 @@ const BatchData = require('../src/models/BatchData')
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+// mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/TRAINERMANGEMENT')
 
 
 exports.getBatches= async(req,res)=>{
