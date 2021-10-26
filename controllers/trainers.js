a2b653a9b93d1df0bf807156cc4576073a4c1f41
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-26 11:34:55 +0530

    Update trainers.js

--- controllers/trainers.js
+++ controllers/trainers.js
@@ -2,7 +2,8 @@ const LoginData = require('../src/models/LoginData')
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+// mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/TRAINERMANGEMENT')
 
 
 exports.getTrainers= async(req,res)=>{
