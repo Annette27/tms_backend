7b50edcb1df737b913370afe6670e2bc873bc7dd
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-26 11:34:53 +0530

    Update courses.js

--- controllers/courses.js
+++ controllers/courses.js
@@ -2,7 +2,8 @@ const CourseData = require('../src/models/CourseData')
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+// mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/TRAINERMANGEMENT')
 
 
 exports.getCourses= async(req,res)=>{
