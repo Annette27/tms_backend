50e5cabef7c67c919686f1de2af46d844bba2320
Annette27 <86048560+Annette27@users.noreply.github.com>
2021-10-26 11:34:45 +0530

    Update allocations.js

--- controllers/allocations.js
+++ controllers/allocations.js
@@ -2,7 +2,8 @@ const AllocData = require('../src/models/AllocData')
 
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+// mongoose.connect("mongodb+srv://userone:userone@libraryfiles.o5pxy.mongodb.net/TRAINERMANGEMENT?retryWrites=true&w=majority",{useUnifiedTopology:true,useNewUrlParser:true});
+mongoose.connect('mongodb://localhost:27017/TRAINERMANGEMENT')
 
 
 exports.getAllocations= async(req,res)=>{
